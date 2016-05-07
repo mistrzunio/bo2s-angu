a652d92b565a2d1efa1134c6f3d335fe5c7c2c68
Wojciech Deneka <Wojciech.Deneka@ge.com>
2016-05-07 11:44:16 +0200

    remover double json parsing

--- static/app.js
+++ static/app.js
@@ -6,7 +6,13 @@ app.config(function($interpolateProvider) {
 
 var scrum = {
       getStoryDetails: function(stringId) {
-          return {"story_points": 7, "story_title": "lorem ipsum easter egg"};
+        if (stringId== "0, 5, 7") return {"story_points": 2, "story_title": "simple story"};
+        if (stringId== "0, 6, 6") return {"story_points": 3, "story_title": "later later later"};
+        if (stringId== "4, 2, 5") return {"story_points": 4, "story_title": "unique job"};
+        if (stringId== "4, 6, 5") return {"story_points": 5, "story_title": "YAY, so many stories"};
+        if (stringId== "5, 0, 1") return {"story_points": 1, "story_title": "yet aANOTHER simple story"};
+        if (stringId== "7, 5, 7") return {"story_points": 6, "story_title": "get life"};
+        return {"story_points": 7, "story_title": "lorem ipsum easter egg"};
       }
 };
 
@@ -21,8 +27,11 @@ function readStories($scope, $http, ocvUrl) {
     $http.get(ocvUrl).success(function(data){
 
 //        var data = "{\n    \"0, 5, 7\": 3,\n    \"0, 6, 6\": 1,\n    \"4, 2, 5\": 3,\n    \"4, 6, 5\": 2,\n    \"5, 0, 1\": 3,\n    \"7, 5, 7\": 2\n}";    
-        ocvResult = JSON.parse(JSON.parse(data));
-        console.log(data);
+//ocvResult = JSON.parse(JSON.parse(data));
+
+        ocvResult = JSON.parse(data);
+        console.log("ocvResult type arter parse is:"+typeof stringValue);
+        
         if (ocvResult) {
             //alert(ocvResult);
             for (var stringId in ocvResult) {
